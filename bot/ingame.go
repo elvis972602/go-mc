bc88869e84d7174202d661f1b5e742632013fb00
elvis <elvis972602@gmail.com>
2025-02-28 21:13:06 +0800

    add handleBundlePackets

--- bot/ingame.go
+++ bot/ingame.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/Tnze/go-mc/data/packetid"
@@ -17,12 +18,48 @@ func (c *Client) HandleGame() error {
 			return err
 		}
 
-		// handle packets
-		err := c.handlePacket(p)
-		if err != nil {
+		if p.ID == int32(packetid.BundleDelimiter) {
+			err := c.handleBundlePackets()
+			if err != nil {
+				return err
+			}
+		} else {
+			// handle packets
+			err := c.handlePacket(p)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (c *Client) handleBundlePackets() (err error) {
+	var packets []pk.Packet
+	var packetNames []string
+	for i := 0; i < 4096; i++ {
+		var p pk.Packet
+		// Read packets
+		if err := c.Conn.ReadPacket(&p); err != nil {
+			return err
+		}
+
+		if p.ID == int32(packetid.BundleDelimiter) {
+			// bundle finished
+			goto handlePackets
+		}
+
+		packets = append(packets, p)
+		packetNames = append(packetNames, packetid.ClientboundPacketID(p.ID).String())
+	}
+	return errors.New("packet number of a bundle out of limit")
+
+handlePackets:
+	for i := range packets {
+		if err := c.handlePacket(packets[i]); err != nil {
 			return err
 		}
 	}
+	return nil
 }
 
 type PacketHandlerError struct {
